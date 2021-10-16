f18f4463114c2a9cf546dd3412d2e85f2367aee2
andreslc47 <andreslc47@hotmail.com>
2021-10-16 13:57:17 -0500

    everything deleted, only zip file remains

--- build/add_markers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Robotics_Project5/src/add_markers
-# Build directory: /home/robond/Robotics_Project5/build/add_markers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot_simulator/turtlebot_stdr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robond/Robotics_Project5/src/turtlebot_simulator/turtlebot_stdr
-# Build directory: /home/robond/Robotics_Project5/build/turtlebot_simulator/turtlebot_stdr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
